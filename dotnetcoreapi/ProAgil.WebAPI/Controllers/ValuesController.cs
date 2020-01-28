907a016097288dd99eac80c84da315c5f9e3eb9f
Heber Nickison Pietrafeza <hpietrafeza@autodoc.com.br>
2020-01-28 14:26:44 -0300

    criando os primeiros metodos e executando no postman

---
+++ dotnetcoreapi/ProAgil.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ProAgil.WebAPI.Model;
+
+namespace ProAgil.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Evento>> Get()
+        {
+            return new Evento[]{
+                new Evento(){
+                    EventoId = 1,
+                    DataEvento = "28/01/2020",
+                    Local = "São Paulo",
+                    Lote = "1º Lote",
+                    QtdPessoas = 500,
+                    Tema = "Curso Asp.Net Core 1"
+                },
+
+                new Evento(){
+                    EventoId = 2,
+                    DataEvento = "29/01/2020",
+                    Local = "São Paulo",
+                    Lote = "2º Lote",
+                    QtdPessoas = 300,
+                    Tema = "Curso Asp.Net WEBApi Core 1"
+                }
+            };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Evento> Get(int id)
+        {
+            return new Evento[]{
+                new Evento(){
+                    EventoId = 1,
+                    DataEvento = "28/01/2020",
+                    Local = "São Paulo",
+                    Lote = "1º Lote",
+                    QtdPessoas = 500,
+                    Tema = "Curso Asp.Net Core 1"
+                },
+
+                new Evento(){
+                    EventoId = 2,
+                    DataEvento = "29/01/2020",
+                    Local = "São Paulo",
+                    Lote = "2º Lote",
+                    QtdPessoas = 300,
+                    Tema = "Curso Asp.Net WEBApi Core 1"
+                }
+            }.FirstOrDefault(x => x.EventoId == id);
+        }
+    }
+}
